5da1b955e90362f233862e859384ce841d987c62
Brian Cheng <brian.cheng.1997@gmail.com>
2019-11-10 20:04:04 -0500

    Update lcd_control.vhd

--- sources_1/new/lcd_control.vhd
+++ sources_1/new/lcd_control.vhd
@@ -4,7 +4,11 @@ use IEEE.NUMERIC_STD.ALL;
 use ieee.std_logic_arith.all;
 
 entity lcd_control is
-    generic(mystring : string := "ABCDE");
+    generic(
+        mystring : string := "Hello World! ";
+        mychar1 : character := 'a';
+        mychar2 : character := 'b'
+    );
     port(
         clk : in std_logic; --50Mhz Clock
         RS : out std_logic; --Instruction / Data Register Selection
@@ -19,60 +23,61 @@ end lcd_control;
 architecture Behavioral of lcd_control is
     type LCD_STATES IS (
         FUNCTION_SET, DISPLAY_ONOFF_CTRL, DISPLAY_CLEAR, ENTRY_MODE_SET,
-        WRITE_CHAR, RETURN_HOME, STALL, toggle_e
+        WRITE_CHAR, RETURN_HOME, STATE_REG, toggle_e
     );
     signal state : LCD_STATES := FUNCTION_SET;
     signal next_state: LCD_STATES;
     signal data_bus: STD_LOGIC_VECTOR(7 downto 0);
-    signal clk_400: std_logic;
-    signal clk_count: integer range 0 to 62500;
+    signal clk2: std_logic;
+    signal clk_count: integer range 0 to 24000; --50Mhz / 400hz / 2 = 62500
     signal index : integer range 1 to mystring'length;
 begin
 
 DB <= data_bus when RW = '0' else "ZZZZZZZZ";
 PSB <= '1'; --parallel data mode
-RST <= '1';
+RST <= '1'; --reset active low
 
-CLK_DIV_540KHZ: process(clk)
+CLK_DIV_400HZ: process(clk)
 begin
     if rising_edge(clk) then
-        if clk_count < 62500 then
+        if clk_count < 24000 then
             clk_count <= clk_count + 1;
         else
             clk_count <= 0;
-            clk_400 <= NOT clk_400;
+            clk2 <= NOT clk2;
         end if;
     end if;
 end process;
 
-
-STATE_MACHINE: process (clk_400)
+STATE_MACHINE: process (clk2, index)
 begin
-    if rising_edge(clk_400) then
+    if rising_edge(clk2) then
     case state is
     when FUNCTION_SET =>
         RS <= '0'; RW <= '0'; EN <= '1';
-        data_bus <= "00110000";
+        data_bus <= "00110000"; --8bit interface, basic instruction set
         state <= toggle_e;
         next_state <= DISPLAY_ONOFF_CTRL;
     when DISPLAY_ONOFF_CTRL =>
         RS <= '0'; RW <= '0'; EN <= '1';
-        data_bus <= "00001100"; --disp, cursor, blink all ON
+        data_bus <= "00001100"; --display on, cursor off, blink off
         state <= toggle_e;
         next_state <= DISPLAY_CLEAR;
     when DISPLAY_CLEAR =>
         RS <= '0'; RW <= '0'; EN <= '1';
-        data_bus <= "00000001";
+        data_bus <= "00000001"; --DDRAM AC gets X"00", fill DDRAM with X"20"
         state <= toggle_e;
         next_state <= ENTRY_MODE_SET;
     when ENTRY_MODE_SET =>
         RS <= '0'; RW <= '0'; EN <= '1';
-        data_bus <= "00000110";
+        data_bus <= "00000110"; --DDRAM AC increase when R/W, no display shift when R/W
         state <= toggle_e;
         next_state <= WRITE_CHAR;
     when WRITE_CHAR =>
         RS <= '1'; RW <= '0'; EN <= '1';
-        data_bus <= std_logic_vector(to_unsigned( natural( character'pos( mystring(index) ) ), 8) ) ;
+        --write to DDRAM
+        --write the individual characters of the string one by one
+        data_bus <= std_logic_vector(to_unsigned( character'pos( mystring(index) ) , 8) ) ;
         state <= toggle_e;
         if (index < mystring'length) then
             index <= index + 1;
@@ -83,13 +88,13 @@ begin
         end if;
     when RETURN_HOME =>
         RS <= '0'; RW <= '0'; EN <= '1';
-        data_bus <= "00000010";
+        data_bus <= "00000010"; --DDRAM AC gets X"00"
         state <= toggle_e;
         next_state <= WRITE_CHAR;
     when toggle_e =>
         EN <= '0';
-        state <= STALL;
-    when STALL =>
+        state <= STATE_REG;
+    when STATE_REG =>
         state <= next_state;
     end case;
     end if;
